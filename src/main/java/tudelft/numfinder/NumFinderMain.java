86c8c0d79d943facaed21b774da7b7bad1125806
Ali Khajoueenejad <ali.knejad@gmail.com>
2023-12-26 14:10:01 -0500

    Update Roman Numbers

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,9 +9,9 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {4, 3, 123, 2, 1, -1});
 
-        System.out.println(nf.getLargest());
-        System.out.println(nf.getSmallest());
+        System.out.println("Greatest number: " + nf.getLargest());
+        System.out.println("Smallest number: " + nf.getSmallest());
     }
 }
